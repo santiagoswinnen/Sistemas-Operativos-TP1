e5754c72b039b0cfc02f306e45921b8c81ee8c54
Santiago Swinnen <sswinnen@itba.edu.ar>
2018-03-28 21:39:04 -0300

    Advances

--- application.c
+++ application.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include "application.h"
 #include "queue.h"
 #define MD5_LEN 16
@@ -12,26 +13,37 @@
 #define SLAVE_NO 10
 
 int applicationMain(int fileNum, char ** files) {
-    int createdSlaves;
-    pid_t parentPid = getpid();
+    int j;
     int i;
+    pid_t parentPid = getpid();
+    int fds[SLAVE_NO];
+    char ** pipeNames = generatePipenames(SLAVE_NO);
 
+    for(j = 0; (j < SLAVE_NO) && (getpid() == parentPid); j++) {
 
-    for(createdSlaves = 0; (createdSlaves < SLAVE_NO) && (getpid() == parentPid); createdSlaves++) {
-            pid_t newPid = fork();
-            if(newPid == 0) {
-                execl("./slave",NULL, NULL);
-                /*TODO: crear el named pipe*/
-            }
+        mkfifo(pipeNames[j],0666);
+        //fds[j] = open(pipeName,O_WRONLY);
+        //close(fds[j]);
+        pid_t newPid = fork();
+        if(newPid == 0) {
+            execl("./slave", pipeNames[j], NULL);
+        }
     }
-    
-    for(i=0; i < fileNum; i++) {
 
+    for(i=0; i < fileNum/2; i++) {
+        if(isFile(files[i])) {
+            int fd = open(pipeNames[i%SLAVE_NO],O_WRONLY);
+            write(fd,files[i],(strlen(files[i])+1)* sizeof(char));
+            close(fd);
+        }
     }
 
-}
+    while("Los pipes no esten todos vacios") {
+        if(i<fileNum) {
+            "Mando nuevos a hashear";
+        }
 
-void manageTasks(Queue requests, ) {
+    }
 
 }
 
@@ -39,4 +51,18 @@ int isFile(const char* file) {
     struct stat buf;
     stat(file, &buf);
     return !S_ISDIR(buf.st_mode);
+}
+
+char ** generatePipenames(int slaves) {
+    char pipeName [11] = "pipeData";
+    char ** ret = malloc(slaves* sizeof(char*));
+    int i;
+    for(i = 0; i < slaves; i++) {
+        pipeName[9] = (char)(i/10);
+        pipeName[10] = (char)(i%10);
+        pipeName[11] = 0;
+        ret[i] = malloc(sizeof(char)*11);
+        strcpy(ret[i],pipeName);
+    }
+    return ret;
 }
--- application.h
+++ application.h
@@ -2,5 +2,6 @@
 #include "queue.h"
 
 int applicationMain(int fileNum,char ** files);
+char ** generatePipenames(int slaves);
 void receiveTask(Queue requests);
 int isFile(const char* file);
