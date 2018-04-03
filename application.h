85adc458824b2dadd4b974b319878014abcc747a
FelipeGorostiaga <fgorostiaga@itba.edu.ar>
2018-04-03 16:12:11 -0300

    x2

--- application.h
+++ application.h
@@ -1,5 +1,5 @@
 #include <sys/types.h>
-#include <bits/semaphore.h>
+
 
 int applicationMain(int fileNum,char ** files);
 void manageChildren(int fileNum, int slaveNumber, char ** files, int * pipesFd, int * returningPipesFd,char * shm_address,key_t key,sem_t * sem);
